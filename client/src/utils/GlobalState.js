e29181b81b15df55f18b9d2f317a3a2050c5650a
Champion113 <sbalbright19@gmail.com>
2021-01-20 11:47:07 -0500

    added shopping cartUI

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -4,16 +4,15 @@ import { useProductReducer } from './reducers';
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-
-
-
 //initial global state w/useProductReducer
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: ''
+  });
     // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
